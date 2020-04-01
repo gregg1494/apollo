95ac73f31c36db534432cd94277b8c80b32b3f84
gregg1494 <gregg1494@gmail.com>
2020-04-01 01:26:17 -0500

    new table

---
+++ src/main/java/com/greggvandycke/Apollo/repositories/TheaterRepository.java
@@ -0,0 +1,7 @@
+package com.greggvandycke.Apollo.repositories;
+
+import com.greggvandycke.Apollo.models.Theater;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TheaterRepository extends CrudRepository<Theater, Long> {
+}
