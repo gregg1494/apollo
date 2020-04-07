00934347ef325f3182cd3da12294dcabe34a19dd
gregg1494 <gregg1494@gmail.com>
2020-04-06 20:13:13 -0500

    Seperated graphql files

--- src/main/java/com/greggvandycke/Apollo/resolvers/mutations/MovieMutation.java
+++ src/main/java/com/greggvandycke/Apollo/resolvers/mutations/MovieMutation.java
@@ -2,51 +2,27 @@ package com.greggvandycke.Apollo.resolvers.mutations;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.greggvandycke.Apollo.models.Movie;
-import com.greggvandycke.Apollo.models.Theater;
-import com.greggvandycke.Apollo.repositories.MovieRepository;
-import com.greggvandycke.Apollo.repositories.TheaterRepository;
+import com.greggvandycke.Apollo.service.MovieService;
 import javassist.NotFoundException;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
-@RequiredArgsConstructor
 public class MovieMutation implements GraphQLMutationResolver {
 
-	private final MovieRepository movieRepository;
+	@Autowired
+	private MovieService movieService;
 
 	public Movie createMovie(String title, int length) {
-		Movie movie = new Movie();
-		movie.setTitle(title);
-		movie.setLength(length);
-		movieRepository.save(movie);
-		return movie;
+		return movieService.createMovie(title, length);
 	}
 
 	public boolean deleteMovie(long id) {
-		movieRepository.deleteById(id);
+		movieService.deleteById(id);
 		return true;
 	}
 
 	public Movie updateMovie(long id, String title, int length) throws NotFoundException {
-		Optional<Movie> optionalMovie = movieRepository.findById(id);
-
-		if (optionalMovie.isPresent()) {
-			Movie movie = optionalMovie.get();
-
-			if (title != null) {
-				movie.setTitle(title);
-			}
-			if(length > 0) {
-				movie.setLength(length);
-			}
-
-			movieRepository.save(movie);
-			return movie;
-		}
-
-		throw new NotFoundException("No found movie to update!");
+		return movieService.updateMovie(id, title, length);
 	}
 }
---
+++ src/main/java/com/greggvandycke/Apollo/service/TheaterService.java
@@ -0,0 +1,58 @@
+package com.greggvandycke.Apollo.service;
+
+import com.greggvandycke.Apollo.models.Theater;
+import com.greggvandycke.Apollo.repositories.TheaterRepository;
+import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TheaterService {
+
+	private final TheaterRepository theaterRepository;
+
+	@Transactional
+	public Theater createTheater(String name, String location) {
+		Theater theater = new Theater();
+		theater.setName(name);
+		theater.setLocation(location);
+		theaterRepository.save(theater);
+		return theater;
+	}
+
+	@Transactional(readOnly = true)
+	public List<Theater> theaters(){
+		return theaterRepository.findAll();
+	}
+
+	@Transactional
+	public void deleteById(long id) {
+		theaterRepository.deleteById(id);
+	}
+
+	@Transactional
+	public Theater updateTheater(long id, String name, String location) throws NotFoundException {
+		Optional<Theater> optionalTheater = theaterRepository.findById(id);
+
+		if (optionalTheater.isPresent()) {
+			Theater theater = optionalTheater.get();
+
+			if(name != null) {
+				theater.setName(name);
+			}
+			if(location != null) {
+				theater.setLocation(location);
+			}
+
+			theaterRepository.save(theater);
+			return theater;
+		}
+
+		throw new NotFoundException("No found theater to update!");
+	}
+}
