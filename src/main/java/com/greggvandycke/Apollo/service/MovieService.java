57dac5dd3883aa20ba9baec44d100123daa5d4ce
gregg1494 <gregg1494@gmail.com>
2020-04-09 23:50:12 -0500

    graph_spqr working

--- src/main/java/com/greggvandycke/Apollo/service/MovieService.java
+++ src/main/java/com/greggvandycke/Apollo/service/MovieService.java
@@ -2,6 +2,9 @@ package com.greggvandycke.Apollo.service;
 
 import com.greggvandycke.Apollo.models.Movie;
 import com.greggvandycke.Apollo.repositories.MovieRepository;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,12 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@GraphQLApi
 @Service
 @AllArgsConstructor
 public class MovieService {
 
 	private MovieRepository movieRepository;
 
+	@GraphQLMutation
 	@Transactional
 	public Movie createMovie(String title, int length) {
 		Movie movie = new Movie();
@@ -25,17 +30,20 @@ public class MovieService {
 		return movie;
 	}
 
+	@GraphQLQuery
 	@Transactional(readOnly = true)
 	public List<Movie> movies(){
 		return movieRepository.findAll();
 	}
 
+	@GraphQLQuery
 	@Transactional
-	public boolean deleteById(long id) {
+	public boolean deleteMovie(long id) {
 		movieRepository.deleteById(id);
 		return true;
 	}
 
+	@GraphQLMutation
 	@Transactional
 	public Movie updateMovie(long id, String title, int length) throws NotFoundException {
 		Optional<Movie> optionalMovie = movieRepository.findById(id);
@@ -57,6 +65,7 @@ public class MovieService {
 		throw new NotFoundException("No found movie to update!");
 	}
 
+	@GraphQLQuery
 	@Transactional(readOnly = true)
 	public Movie getMovie(long id) {
 		Optional<Movie> optionalMovie = movieRepository.findById(id);
