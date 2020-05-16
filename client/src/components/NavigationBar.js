0015e70bcb8fb8c6755f52e7ec0db5f17770be0b
gregg1494@gmail.com <gregg1494@gmail.com>
2020-05-16 14:06:04 -0500

    bootstrap navbar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Navbar, Nav} from "react-bootstrap";
 
 class NavigationBar extends React.Component {
     render() {
-        return <div>NavigationBar</div>;
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home"> <img src="http://www.emoji.co.uk/files/phantom-open-emojis/objects-phantom/12785-movie-camera.png"  width="30" height="30" alt="brand"/> Apollo </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#movies">Movies</Nav.Link>
+                    <Nav.Link href="#theaters">Theaters</Nav.Link>
+                </Nav>
+            </Navbar>
+        );
     }
 }
 
