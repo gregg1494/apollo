157d45f08959438930cfd1b9d820f1b722afd79e
gregg1494@gmail.com <gregg1494@gmail.com>
2020-05-21 14:12:22 -0500

    revert

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 import {Card, Table} from 'react-bootstrap';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faList} from '@fortawesome/free-solid-svg-icons'
 
 export default class Movies extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        };
+    }
+
+    componentDidMount() {
+        axios({
+            url: 'http://localhost:8080/graphql',
+            method: 'get',
+            data: {
+                query: `
+                  query movies {
+                        id
+                        title
+                        length
+                    }
+                  `
+            }
+        }).then((result) => {
+            console.log(result.data)
+        });
+    }
+
     render() {
         return (
             <Card className={'boarder boarder-dark bg-dark text-white'}>
