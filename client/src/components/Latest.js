2488b185e036597afe27b6450d4da41e3908f768
gregg1494@gmail.com <gregg1494@gmail.com>
2020-06-12 20:32:27 -0500

    update

--- client/src/components/Latest.js
+++ client/src/components/Latest.js
@@ -1,58 +1,10 @@
 import React, {Component} from 'react';
-import { Player, ControlBar } from 'video-react';
-import axios from "axios";
 
 export default class Latest extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            url: null,
-            errors: []
-        }
-    }
-
-    componentDidMount() {
-
-        const query = `query getMovie {
-                            movie(id: 4) {
-                                title
-                                url
-                            }
-                       }`;
-
-        const variables = {id: 4}
-
-        this.getMovie(query, variables).catch(error => console.log(error));
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-        this.setState({url: null});
-    }
-
-    getMovie = async (query, variables) => {
-        try {
-            await axios.post(process.env.REACT_APP_API_ENDPOINT, {
-                query,
-                variables
-            }).then(response => {
-                console.log(response.data.data.movie.title);
-                console.log(response.data.data.movie.url);
-
-                this.setState({url: response.data.data.movie.url});
-                console.log(this.state.url);
-            });
-        } catch (error) {
-            this.state(() => ({ error }));
-        }
-    }
-
     render () {
         return (
-            <Player autoPlay src={this.state.url}>
-                <ControlBar autoHide={true} className="my-class" />
-            </Player>
+            <h1>Latest</h1>
         );
     }
 }
--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,120 +1,58 @@
 import React, {Component} from 'react';
-import {Card, Col, Form, Button} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlusCircle, faSave, faUndo} from '@fortawesome/free-solid-svg-icons'
-import axios from 'axios';
+import { Player, ControlBar } from 'video-react';
+import axios from "axios";
 
-export default class Movie extends Component {
+export default class Latest extends Component {
 
     constructor(props) {
         super(props);
-        this.state = this.initialState;
-        this.movieChange = this.movieChange.bind(this);
-        this.submitForm = this.submitForm.bind(this);
-    }
-
-    initialState = {
-        id: '',
-        title: '',
-        length: ''
-    }
-
-    resetState = () => {
-        this.setState(() => this.initialState);
-    }
-
-    submitForm = (event) => {
-        event.preventDefault();
-
-        const movie = {
-            id: this.state.id,
-            title: this.state.title,
-            length: this.state.length
-        };
-
-        const query = `mutation createMovie($title:String!, $length:Int!) {
-                            createMovie(title:$title ,length:$length) {
-                                id
-                              }
-                            }`
-
-        const variables = {title: movie.title, length: movie.length};
-
-        this.createMovie(query, variables).catch(error => console.log(error));
-
-        this.resetState();
-
-    }
-
-    movieChange = (event) => {
-        this.setState({
-            [event.target.name]:event.target.value
-        });
-    }
-
-    createMovie = async (query, variables) => {
-        try {
-            const response = await axios.post(process.env.REACT_APP_API_ENDPOINT, {
-                query,
-                variables
-            });
-
-            if(response.data != null) {
-                console.log("Movie saved!")
-            }
-
-        } catch (error) {
-            console.log(error);
+        this.state = {
+            url: null,
+            errors: []
         }
     }
 
-    render() {
-        const {id, title, length} = this.state;
+    componentDidMount() {
 
+        const query = `query getMovie {
+                            movie(id: 4) {
+                                title
+                                url
+                            }
+                       }`;
+
+        const variables = {id: 4}
+
+        this.getMovie(query, variables).catch(error => console.log(error));
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+        this.setState({url: null});
+    }
+
+    getMovie = async (query, variables) => {
+        try {
+            await axios.post(process.env.REACT_APP_API_ENDPOINT, {
+                query,
+                variables
+            }).then(response => {
+                console.log(response.data.data.movie.title);
+                console.log(response.data.data.movie.url);
+
+                this.setState({url: response.data.data.movie.url});
+                console.log(this.state.url);
+            });
+        } catch (error) {
+            this.state(() => ({ error }));
+        }
+    }
+
+    render () {
         return (
-            <Card className={"boarder boarder-dark bg-dark text-white"}>
-                <Card.Header><FontAwesomeIcon icon={faPlusCircle}/> Movie </Card.Header>
-                <Form onSubmit={this.submitForm} onReset={this.resetState} id="movieFormId">
-                    <Card.Body>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGroupId">
-                                <Form.Label>Id</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="test"
-                                    name="id"
-                                    value={id} onChange={this.movieChange}
-                                    className={"bg-dark text-white"}/>
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGroupTitle">
-                                <Form.Label>Title</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="test"
-                                    name="title"
-                                    value={title} onChange={this.movieChange}
-                                    className={"bg-dark text-white"}/>
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGroupLength">
-                                <Form.Label>Length</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="test"
-                                    name="length"
-                                    value={length} onChange={this.movieChange}
-                                    className={"bg-dark text-white"}/>
-                            </Form.Group>
-                        </Form.Row>
-                    </Card.Body>
-                    <Card.Footer style={{"textAlign":"right"}}>
-                        <Button size={"sm"} variant="success" type="submit">
-                            <FontAwesomeIcon icon={faSave}/> Submit
-                        </Button>{' '}
-                        <Button size={"sm"} variant="info" type="reset">
-                            <FontAwesomeIcon icon={faUndo}/> Reset
-                        </Button>
-                    </Card.Footer>
-                </Form>
-            </Card>
-        )
+            <Player autoPlay src={this.state.url}>
+                <ControlBar autoHide={true} className="my-class" />
+            </Player>
+        );
     }
 }
